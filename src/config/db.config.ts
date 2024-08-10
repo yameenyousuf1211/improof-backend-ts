0f6b586123aac1691f86d1d03500cf3e9e0da902
ali usaid <aliusaid55@gmail.com>
2024-08-10 08:59:24 -0700

    fix index unique issue

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -6,7 +6,7 @@ export const connectDB = async () => {
         const DB_URI = `${process.env.MONGODB_URL}/${process.env.DB_NAME}`;
         const conn = await mongoose.connect(DB_URI as string);
         console.log(`DB connected -> ${conn.connection.name}`);
-
+        
         // create default admin
         await createDefaultAdmin();
     } catch (error: any) {
